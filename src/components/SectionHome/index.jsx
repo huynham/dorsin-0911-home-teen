3da783d9eebe280cc6b25e365c7cb63bab720434
huynham <91304084+huynham@users.noreply.github.com>
2021-11-23 16:13:36 +0700

    first commit

---
+++ src/components/SectionHome/index.jsx
@@ -0,0 +1,64 @@
+
+import './home.css'
+import wave from '../../image/bg-pattern.png'
+import Particles from 'react-particles-js';
+
+
+function Home(){
+   
+
+    
+    return (
+        <section id="home">
+           <div className="particle">
+                    <Particles
+                        params={{
+                        "particles": {
+                            "number": {
+                                "value": 200,
+                            },
+                            "size": {
+                                "value": 5,
+                            }
+                        },
+                        "move": {
+                            "enable":true,
+
+                        },
+                        "interactivity": {
+                            "events": {
+                                "onhover": {
+                                    "enable": true,
+                                    "mode": "repulse",
+                                }
+                            }
+                        }
+                    }} />
+                   
+                   </div>
+            <div className="home-sub">
+                           
+                <div className="home-sub-one">
+                    <div className="home-content">
+                        <h1>We help startups launch their products</h1>
+                        <p>Etiam sed.Interdum consequat proin vestibulum class at.</p>
+                        <span><i class="fal fa-play-circle"></i></span>
+                        
+                        
+                    </div>
+                </div>
+                
+            </div>
+            <div className="wave">
+                    <img src= {wave} alt="song" />
+
+            </div>
+            <div className="video">
+                
+                </div>
+           
+           
+        </section>
+    )
+}
+export default Home
---
+++ src/components/SectionSetting/setting.jsx
@@ -0,0 +1,36 @@
+import './setting.css';
+
+function Setting(){
+    
+    
+    return (
+
+        <section id="setting">
+            <div className="setting">
+                <div className="setting-sub">
+                    <i class="far fa-cog"></i>
+                    
+
+                </div>
+                <div className="setting-color">
+                    
+                    <span className="sp1"></span>
+                    <span className="sp2"></span>
+                    <span className="sp3"></span>
+                    <span className="sp4"></span>
+                    <span className="sp5"></span>
+                    <span className="sp6"></span>
+                    <span className="sp7"></span>
+                    <span className="sp8"></span>
+                    <span className="sp9"></span>
+
+                </div>
+            </div>
+            
+        </section>
+
+
+    )
+}
+
+export default Setting
